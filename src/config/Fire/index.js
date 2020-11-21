59ed43866c26a43a3686f625386b409c81171d1c
DIMAS ADITYA PRATAMA <48445708+dimasadityapratama@users.noreply.github.com>
2020-11-21 20:31:35 +0700

    Initial commit

---
+++ src/config/Fire/index.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB3bWoA4H-jpbHtxbE_cNO0AEmGJtpArxE",
+  authDomain: "mybangdims.firebaseapp.com",
+  databaseURL: "https://mybangdims.firebaseio.com",
+  projectId: "mybangdims",
+  storageBucket: "mybangdims.appspot.com",
+  messagingSenderId: "410582256217",
+  appId: "1:410582256217:web:32386a806955468f547f48",
+  measurementId: "G-RJVBC0TPRW"
+});
+
+const Fire = firebase;
+
+export default Fire;
