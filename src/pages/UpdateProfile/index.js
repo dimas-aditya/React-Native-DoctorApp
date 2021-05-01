a080cc7c931b5bac0e20f836aa900bebfb48c5fe
DIMAS ADITYA PRATAMA <48445708+dimasadityapratama@users.noreply.github.com>
2021-05-01 13:37:31 +0700

    update
    update

--- src/pages/UpdateProfile/index.js
+++ src/pages/UpdateProfile/index.js
@@ -1,12 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import {ScrollView, StyleSheet, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
-import {ILNullPhoto} from '../../assets';
-import {Button, Gap, Header, Input, Profile} from '../../components';
-import {Fire} from '../../config';
-import {colors, getData, showError, storeData} from '../../utils';
+import { ILNullPhoto } from '../../assets';
+import { Button, Gap, Header, Input, Profile } from '../../components';
+import { Fire } from '../../config';
+import { colors, getData, showError, storeData } from '../../utils';
 
-const UpdateProfile = ({navigation}) => {
+const UpdateProfile = ({ navigation }) => {
   const [profile, setProfile] = useState({
     fullName: '',
     profession: '',
@@ -20,7 +20,7 @@ const UpdateProfile = ({navigation}) => {
     getData('user').then(res => {
       const data = res;
       data.photoForDB = res?.photo?.length > 1 ? res.photo : ILNullPhoto;
-      const tempPhoto = res?.photo?.length > 1 ? {uri: res.photo} : ILNullPhoto;
+      const tempPhoto = res?.photo?.length > 1 ? { uri: res.photo } : ILNullPhoto;
       setPhoto(tempPhoto);
       setProfile(data);
     });
@@ -78,12 +78,12 @@ const UpdateProfile = ({navigation}) => {
 
   const getImage = () => {
     ImagePicker.launchImageLibrary(
-      {quality: 0.5, maxWidth: 200, maxHeight: 200},
+      { quality: 0.5, maxWidth: 200, maxHeight: 200 },
       response => {
         if (response.didCancel || response.error) {
           showError('oops, sepertinya anda tidak memilih foto nya?');
         } else {
-          const source = {uri: response.uri};
+          const source = { uri: response.uri };
 
           setPhotoForDB(`data:${response.type};base64, ${response.data}`);
           setPhoto(source);
@@ -129,6 +129,6 @@ const UpdateProfile = ({navigation}) => {
 export default UpdateProfile;
 
 const styles = StyleSheet.create({
-  page: {backgroundColor: colors.white, flex: 1},
-  content: {padding: 40, paddingTop: 0},
+  page: { backgroundColor: colors.white, flex: 1 },
+  content: { padding: 40, paddingTop: 0 },
 });
